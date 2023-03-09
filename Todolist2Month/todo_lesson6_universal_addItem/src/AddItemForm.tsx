e06be15b08a2bcd4a628b1299e43a4782a30a88f
Kozlov Valentin <kozlov0020@gmail.com>
2023-03-09 20:43:51 +0500

    crud a new state,add useeffect for transformation state from server

---
+++ Todolist2Month/todo_lesson6_universal_addItem/src/AddItemForm.tsx
@@ -0,0 +1,53 @@
+import React, {ChangeEvent, FC, KeyboardEvent, useState} from 'react';
+
+type AddItemFormType = {
+    maxLengthUserMessage: number
+    addNewItem: (title: string) => void
+}
+export const AddItemForm: FC<AddItemFormType> = (
+    {
+        maxLengthUserMessage,
+        addNewItem
+    }) => {
+    const [title, setTitle] = useState<string>("")
+    const [error, setError] = useState<boolean>(false)
+
+    const changeLocalTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        error && setError(false)
+        setTitle(e.currentTarget.value)
+    }
+    const addItem = () => {
+        const trimmedTitle = title.trim()
+        if (trimmedTitle) {
+            addNewItem(trimmedTitle)
+        } else {
+            setError(true)
+        }
+        setTitle("")
+    }
+    const onKeyDownAddItem = (e: KeyboardEvent<HTMLInputElement>) => e.key === "Enter" && addItem()
+
+    const userErrorMessage = error && <div style={{color: "hotpink"}}>Title is required!</div>
+    const isUserMessageToLong: boolean = title.length > maxLengthUserMessage
+    const isAddBtnDisabled = !title.length || isUserMessageToLong || error
+    const userMaxLengthMessage =
+        isUserMessageToLong && <div style={{color: "hotpink"}}>Task title is to long!</div>
+    const inputErrorClasses = error || isUserMessageToLong ? "input-error" : ""
+// const onKeyDownAddItemHandler=isAddBtnDisabled
+
+    return (
+        <div>
+            <input
+                value={title}
+                placeholder="Please, enter title"
+                onChange={changeLocalTitle}
+                onKeyDown={onKeyDownAddItem}
+                className={inputErrorClasses}
+            />
+            <button disabled={isAddBtnDisabled} onClick={addItem}>+</button>
+            {userMaxLengthMessage}
+            {userErrorMessage}
+        </div>
+    );
+};
+
