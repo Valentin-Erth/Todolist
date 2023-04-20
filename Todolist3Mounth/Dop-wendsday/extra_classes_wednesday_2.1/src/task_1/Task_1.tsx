e54d5a1a2aed116d80c6ceebaf2b4b713b67ef68
Kozlov Valentin <kozlov0020@gmail.com>
2023-04-20 12:28:26 +0500

    make task for optimization not re-render

--- Todolist3Mounth/Dop-wendsday/extra_classes_wednesday_2.1/src/task_1/Task_1.tsx
+++ Todolist3Mounth/Dop-wendsday/extra_classes_wednesday_2.1/src/task_1/Task_1.tsx
@@ -1,4 +1,5 @@
-import {ChangeEvent, FC, useCallback, useState} from 'react';
+import {ChangeEvent, FC, memo, useCallback, useState} from 'react';
+
 
 const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
 
@@ -8,25 +9,30 @@ type PropsType = { value: string, onChange: (e: ChangeEvent<HTMLInputElement>) =
 // If change value on input Title component not re-render
 // If change value on input Task_1 component not re-render
 
-export const Task_1 = () => {
-  const [value, setValue] = useState('');
+export const Task_1 = memo(() => {
+    console.log("Task_1 is called")
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value);
-  };
+    return (
+        <div style={{...CONTAINER_STYLES} as any}>
+            <Input />
+            <Title title="I am a title"/>
+        </div>
+    );
+});
 
-  return (
-    <div style={{...CONTAINER_STYLES} as any}>
-      <Input value={value} onChange={handleChange} />
-      <Title title="I am a title" />
-    </div>
-  );
-};
+const Title = memo((props: { title: string }) => {
+    console.log("Title is called")
+    return (
+        <h3>{props.title}</h3>
+    )
+})
 
-const Title = (props: { title: string }) => <h3>{props.title}</h3>;
-
-const Input: FC<PropsType> = ({value, onChange}) => {
-  return (
-    <input type="text" placeholder="Placeholder" value={value} onChange={onChange} />
-  );
+const Input: FC = () => {
+    const [value, setValue] = useState('');
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value);
+    }
+    return (
+        <input type="text" placeholder="Placeholder" value={value} onChange={handleChange}/>
+    );
 };
